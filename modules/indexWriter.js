f3d3115a3e0f2bd3e8ade5dff412057e9e19df0b
Victor Fernandes <victorfern91@gmail.com>
2017-01-20 10:16:29 +0000

    update code to work with requireJS structure

--- modules/indexWriter.js
+++ modules/indexWriter.js
@@ -1,26 +1,30 @@
 const fs = require('fs')
 
 module.exports = class indexWriter {
-  constructor(indexFilePath) {
-    this.path = indexFilePath;
-    this.addHotReloadScript();
-  }
+    constructor(indexFilePath) {
+        this.path = indexFilePath;
+        this.addHotReloadScript();
+    }
 
-  getIndexContent () {
-    this.addHotReloadScript()
-    return this.indexContent;
-  }
+    getIndexContent() {
+        this.addHotReloadScript()
+        return this.indexContent;
+    }
 
-  generateScriptTag (scriptPath) {
-    return `<script src="${scriptPath}"></script>\n`;
-  }
+    generateScriptTag(scriptPath) {
+        return `<script src="${scriptPath}"></script>\n`;
+    }
 
-  addHotReloadScript () {
-    this.indexContent = fs.readFileSync(this.path, 'utf8');
-    let socketIO = this.generateScriptTag('/socket.io/socket.io.js'),
-        hotReloadScript = this.generateScriptTag('/scripts/hot-reload-client.js'),
-        position = this.indexContent.indexOf('</body>');
+    addHotReloadScript() {
+        this.indexContent = fs.readFileSync(this.path, 'utf8');
+        let socketIO = this.generateScriptTag('/socket.io/socket.io.js'),
+            hotReloadScript = this.generateScriptTag('/scripts/hot-reload-client.js'),
+            position = this.indexContent.indexOf('<script');
 
-    this.indexContent = this.indexContent.substr(0, position) + socketIO + hotReloadScript + this.indexContent.substr(position)
-  }
+        if (position < 0) {
+            position = this.indexContent.indexOf('</body>');
+        }
+
+        this.indexContent = this.indexContent.substr(0, position) + socketIO + hotReloadScript + this.indexContent.substr(position)
+    }
 }
